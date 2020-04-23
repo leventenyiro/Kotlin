49acc15c00dd0495664488ebf444aa54262860a9
Leviii99 <nyiro.levente@gmail.com>
2020-04-23 16:18:54 +0200

    Main2Activity

--- app/src/main/java/com/leventenyiro/kotlin/Main2Activity.kt
+++ app/src/main/java/com/leventenyiro/kotlin/Main2Activity.kt
@@ -1,11 +1,19 @@
 package com.leventenyiro.kotlin
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class Main2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        btnBack.setOnClickListener {
+            val intent = Intent(this@Main2Activity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
